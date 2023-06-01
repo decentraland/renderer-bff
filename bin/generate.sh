0c45d2cb43a29a50abf292fd327b0771d1050ac9
Hugo Arregui <hugo@decentraland.org>
2023-06-01 13:34:29 -0300

    try to generate protobuf from openapi

---
+++ bin/generate.sh
@@ -0,0 +1,2 @@
+# npx @openapitools/openapi-generator-cli generate -i node_modules/@dcl/catalyst-api-specs/lib/api.yaml -g protobuf-schema -o proto
+protoc --plugin=./node_modules/.bin/protoc-gen-dcl_ts_proto --ts_proto_out=. -I=./proto/models ./proto/models/entity.proto
